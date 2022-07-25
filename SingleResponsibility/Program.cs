35c70b16165261ca2d5dfe22a7905894baa855c0
t.novruzov <t.novruzov@STHQ0116-01>
2022-07-25 21:54:43 +0400

    Add project files.

---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace SingleResponsibility;
+
+// SOLID Principles
+
+
+
+// Bad Example
+
+/*
+public class SendEmail
+{
+    public bool Send(string mail, string message)
+    {
+        if (!IsMailAddressValid(mail))
+        {
+            return false;
+        }
+
+
+        // Send mail
+        return true;
+    }
+
+
+    public bool IsMailAddressValid(string mail)
+    {
+        // check (Regex)
+        return true;
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+// Good Example
+
+public class MailAddressValidation
+{
+    public bool IsMailAddressValid(string mail)
+    {
+        // check (Regex)
+        return true;
+    }
+}
+
+
+
+public class SendEmail
+{
+    private MailAddressValidation _validation = new();
+    public bool Send(string mail, string message)
+    {
+        if (!_validation.IsMailAddressValid(mail))
+        {
+            return false;
+        }
+
+        // Send mail
+        return true;
+    }
+}
